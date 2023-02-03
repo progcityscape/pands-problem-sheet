bee060a1541aebeb16b9ce98a33cacfbd606b477
progscape <G00426048@atu.ie>
2023-02-03 19:12:13 +0000

    Topic/Week 2

---
+++ bank.py
@@ -0,0 +1,17 @@
+# this program will read two integer amounts and add them together to create an amount in currency format.
+amount1 = int(input ("Enter Amount 1 (in cents): "))
+amount2 = int(input ("Enter Amount 1 (in cents): "))
+print ("Thank you.  Calculating...")
+total = amount1 + amount2
+total2 = str(total)
+
+#parse cent amount to euro and cent
+euroTotal = ((int (total2))/100)
+
+#specify decimal places for euroTotal
+#https://www.freecodecamp.org/news/2f-in-python-what-does-it-mean/#:~:text=In%20Python%2C%20there%20are%20various,point%20number%20is%20rounded%20up.
+
+decimalplace = "%.2f" % euroTotal
+#output amount in human readable form
+print ("The sum of these is " + "€" + str(decimalplace))
+
