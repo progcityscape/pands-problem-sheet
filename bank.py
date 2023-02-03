072f8b40f1f80e95e13bf68d169a8c8209d52513
progscape <G00426048@atu.ie>
2023-02-03 19:16:25 +0000

    /Week 2 Task

--- bank.py
+++ bank.py
@@ -3,10 +3,9 @@ amount1 = int(input ("Enter Amount 1 (in cents): "))
 amount2 = int(input ("Enter Amount 1 (in cents): "))
 print ("Thank you.  Calculating...")
 total = amount1 + amount2
-total2 = str(total)
 
 #parse cent amount to euro and cent
-euroTotal = ((int (total2))/100)
+euroTotal = ((int (total))/100)
 
 #specify decimal places for euroTotal
 #https://www.freecodecamp.org/news/2f-in-python-what-does-it-mean/#:~:text=In%20Python%2C%20there%20are%20various,point%20number%20is%20rounded%20up.
